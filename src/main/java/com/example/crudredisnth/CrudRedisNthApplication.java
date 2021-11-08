7e58afcbcc9e0a2862cbd3d1bf73c8f463297086
Hau Nguyen <thehaung217@gmail.com>
2021-11-09 06:43:14 +0700

    Initial commit

---
+++ src/main/java/com/example/crudredisnth/CrudRedisNthApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudredisnth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudRedisNthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudRedisNthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crudredisnth/CrudRedisNthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudredisnth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRedisNthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
